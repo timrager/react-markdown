bcadaac26e4975671b1f74b528384b25ed511238
Tim Rager <tim.rager@gmail.com>
2022-01-16 12:59:16 -0800

    added localStorage for persistence

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ import { nanoid } from "nanoid"
 import './style.css';
 
 export default function App() {
-    const [notes, setNotes] = React.useState([])
-    const [currentNoteId, setCurrentNoteId] = React.useState(
-        (notes[0] && notes[0].id) || ""
-    )
+
+    // Set State for Notes and ID for Notes
+    const [notes, setNotes] = React.useState( JSON.parse(localStorage.getItem("notes")) || [])
+    const [currentNoteId, setCurrentNoteId] = React.useState( (notes[0] && notes[0].id) || "" )
     
+    React.useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes))
+    }, [notes])
+
     function createNewNote() {
         const newNote = {
             id: nanoid(),
